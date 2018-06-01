e70824bc8021c5aba077e9ec89d99fa1fcf6705a
drudo1 <drudo1@gmail.com>
2018-06-01 14:11:22 -0500

    added logging

--- MunchkinMonitor/Classes/Logger.cs
+++ MunchkinMonitor/Classes/Logger.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace MunchkinMonitor.Classes
 {
@@ -19,6 +21,25 @@ namespace MunchkinMonitor.Classes
             RoomState state = RoomState.CurrentState;
             state.playerStats.LogVictory(playerID);
             GameStats.LogVictory(playerID);
+        }
+        internal static void LogError(Exception ex)
+        {
+            string path = HttpContext.Current.Server.MapPath("~/") + "errors.xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Exception>));
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            List<Exception> exList;
+            using (FileStream stream = File.OpenRead(path))
+            {
+                exList = ((List<Exception>)serializer.Deserialize(stream)).ToList();
+            }
+            exList.Add(ex);
+            using (FileStream stream = File.OpenWrite(path))
+            {
+                serializer.Serialize(stream, exList);
+            }
         }
     }
 }
