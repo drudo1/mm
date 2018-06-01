da6535ee0b92add33eb9da88ff61c45fa1745e87
drudo1 <drudo1@gmail.com>
2018-06-01 14:23:38 -0500

    made exception serializable

--- MunchkinMonitor/Classes/Logger.cs
+++ MunchkinMonitor/Classes/Logger.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -24,22 +26,80 @@ namespace MunchkinMonitor.Classes
         }
         internal static void LogError(Exception ex)
         {
+            SerializableException sEx = new SerializableException(ex);
+
             string path = HttpContext.Current.Server.MapPath("~/") + "errors.xml";
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Exception>));
+            XmlSerializer serializer = new XmlSerializer(typeof(List<SerializableException>));
             if (!File.Exists(path))
             {
                 File.Create(path);
             }
-            List<Exception> exList;
+            List<SerializableException> exList;
             using (FileStream stream = File.OpenRead(path))
             {
-                exList = ((List<Exception>)serializer.Deserialize(stream)).ToList();
+                exList = ((List<SerializableException>)serializer.Deserialize(stream)).ToList();
             }
-            exList.Add(ex);
+            exList.Add(sEx);
             using (FileStream stream = File.OpenWrite(path))
             {
                 serializer.Serialize(stream, exList);
             }
         }
+    }
+
+    [Serializable]
+    public class SerializableException
+    {
+        #region Members
+        private KeyValuePair<object, object>[] _Data; //This is the reason this class exists. Turning an IDictionary into a serializable object
+        private string _HelpLink = string.Empty;
+        private SerializableException _InnerException;
+        private string _Message = string.Empty;
+        private string _Source = string.Empty;
+        private string _StackTrace = string.Empty;
+        #endregion
+
+        #region Constructors
+        public SerializableException()
+        {
+        }
+
+        public SerializableException(Exception exception) : this()
+        {
+            setValues(exception);
+        }
+        #endregion
+
+        #region Properties
+        public string HelpLink { get { return _HelpLink; } set { _HelpLink = value; } }
+        public string Message { get { return _Message; } set { _Message = value; } }
+        public string Source { get { return _Source; } set { _Source = value; } }
+        public string StackTrace { get { return _StackTrace; } set { _StackTrace = value; } }
+        public SerializableException InnerException { get { return _InnerException; } set { _InnerException = value; } } // Allow null to be returned, so serialization doesn't cascade until an out of memory exception occurs
+        public KeyValuePair<object, object>[] Data { get { return _Data ?? new KeyValuePair<object, object>[0]; } set { _Data = value; } }
+        #endregion
+
+        #region Private Methods
+        private void setValues(Exception exception)
+        {
+            if (null != exception)
+            {
+                _HelpLink = exception.HelpLink ?? string.Empty;
+                _Message = exception.Message ?? string.Empty;
+                _Source = exception.Source ?? string.Empty;
+                _StackTrace = exception.StackTrace ?? string.Empty;
+                setData(exception.Data);
+                _InnerException = new SerializableException(exception.InnerException);
+            }
+        }
+
+        private void setData(ICollection collection)
+        {
+            _Data = new KeyValuePair<object, object>[0];
+
+            if (null != collection)
+                collection.CopyTo(_Data, 0);
+        }
+        #endregion
     }
 }
